a9b5f21dd6147ac5eda9c740a9db565ae56db7ce
Luisa <luisafmv321@gmail.com>
2023-02-24 10:50:57 -0500

    Versión 1

---
+++ main.js
@@ -0,0 +1,109 @@
+/* //1
+let miNombre = 'Luisa';
+//2
+let miApellido = 'Mendoza';
+//3
+let miEdad = 20;
+//4
+let miMascota = 'Haru';
+//5
+let edadMascota = 2;
+//6
+console.log(miNombre);
+console.log(miApellido);
+console.log(miEdad);
+console.log(miMascota);
+console.log(edadMascota);
+//7
+let nombreCompleto = miNombre +' '+ miApellido;
+console.log(nombreCompleto);
+//8
+let textoPresentacion = 'Buenos días, mi nombre es ' + nombreCompleto + ', tengo ' + miEdad + ' años' +'. Mi perrita se llama ' + miMascota + ' y tiene ' + edadMascota + ' años.';
+console.log(textoPresentacion);
+//9
+let sumaEdades = miEdad + edadMascota;
+console.log(sumaEdades);
+let restaEdades = miEdad - edadMascota;
+console.log(restaEdades);
+let productoEdades = miEdad * edadMascota;
+console.log(productoEdades);
+let divisionEdades = miEdad / edadMascota;
+console.log(divisionEdades);
+//10
+miNombre = prompt('¿Cúal es tu nombre?');
+console.log(miNombre);
+
+miApellido = prompt('¿Cúal es tu apellido?');
+console.log(miApellido);
+
+miEdad = Number(prompt('¿Cúal es tu edad?'));
+console.log(miEdad);
+
+miMascota = prompt('¿Cúal es el nombre de tu mascota?');
+console.log(miMascota);
+
+edadMascota = Number(prompt('¿Cúal es la edad de tu mascota?'));
+console.log(edadMascota);
+
+nombreCompleto = miNombre +' '+ miApellido
+console.log(nombreCompleto);
+
+textoPresentacion = 'Buenos días, mi nombre es ' + nombreCompleto + ', tengo ' + miEdad + ' años' +'. Mi perrita se llama ' + miMascota + ' y tiene ' + edadMascota + ' años.';
+console.log(textoPresentacion);
+
+sumaEdades = miEdad + edadMascota;
+console.log(sumaEdades);
+
+restaEdades = miEdad - edadMascota;
+console.log(restaEdades);
+
+productoEdades = miEdad * edadMascota;
+console.log(productoEdades);
+
+divisionEdades = miEdad / edadMascota;
+console.log(divisionEdades); */
+//11
+/* const alumno = {
+nombre: 'Luisa',
+apellido: 'Mendoza',
+edad: 20,
+altura: '1.63 cm'
+pais: 'Colombia',
+ciudad: 'Medellín',
+}
+console.table(alumno);
+console.log(alumno.nombre);
+console.log(alumno.apellido);
+console.log(alumno.edad);
+console.log(alumno.pais);
+console.log(alumno.ciudad);
+//12
+const mascota ={
+    nombre:'Haru',
+    raza:'criolla',
+    color:'negro',
+    sexo:'hembra',
+    edad: '2 años'
+}
+console.table(mascota);
+console.log(mascota.nombre);
+console.log(mascota.raza);
+console.log(mascota.color);
+console.log(mascota.sexo);
+console.log(mascota.edad); */
+//13
+let frutas = ['manzana','fresa','mango','sandía','pera'];
+console.log(frutas);
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+console.log(frutas[3]);
+console.log(frutas[4]);
+//14
+let numeros = [5,15,25,35,45];
+console.log(numeros);
+console.log(numeros[0]);
+console.log(numeros[1]);
+console.log(numeros[2]);
+console.log(numeros[3]);
+console.log(numeros[4]);
