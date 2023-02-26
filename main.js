91dd70365725da535bc976bad3f31d3472837ccb
Luisa <luisafmv321@gmail.com>
2023-02-25 23:11:20 -0500

    Version 2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* //1
+///1
 let miNombre = 'Luisa';
 //2
 let miApellido = 'Mendoza';
@@ -61,16 +61,16 @@ productoEdades = miEdad * edadMascota;
 console.log(productoEdades);
 
 divisionEdades = miEdad / edadMascota;
-console.log(divisionEdades); */
+console.log(divisionEdades);
 //11
-/* const alumno = {
+const alumno = {
 nombre: 'Luisa',
 apellido: 'Mendoza',
-edad: 20,
-altura: '1.63 cm'
+edad: '20 años',
+altura: '1.63 m',
 pais: 'Colombia',
 ciudad: 'Medellín',
-}
+};
 console.table(alumno);
 console.log(alumno.nombre);
 console.log(alumno.apellido);
@@ -79,18 +79,18 @@ console.log(alumno.pais);
 console.log(alumno.ciudad);
 //12
 const mascota ={
-    nombre:'Haru',
-    raza:'criolla',
-    color:'negro',
-    sexo:'hembra',
-    edad: '2 años'
-}
+nombre:'Haru',
+raza:'criolla',
+color:'negro',
+sexo:'hembra',
+edad: '2 años'
+};
 console.table(mascota);
 console.log(mascota.nombre);
 console.log(mascota.raza);
 console.log(mascota.color);
 console.log(mascota.sexo);
-console.log(mascota.edad); */
+console.log(mascota.edad);
 //13
 let frutas = ['manzana','fresa','mango','sandía','pera'];
 console.log(frutas);
@@ -107,3 +107,65 @@ console.log(numeros[1]);
 console.log(numeros[2]);
 console.log(numeros[3]);
 console.log(numeros[4]);
+//15
+const familia = [
+    {
+        nombre:'Luisa',
+        apellido: 'Mendoza',
+        edad:'20 años'
+    },
+    {
+        nombre:'Luis',
+        apellido: 'Mendoza',
+        edad:'61 años' 
+    },
+    {
+        nombre:'Yolanda',
+        apellido: 'Valencia',
+        edad:'55 años' 
+    },  
+    {  
+        nombre:'Fernanda',
+        apellido: 'Valencia',
+        edad:'20 años' 
+    },
+    {
+        nombre:'Haru',
+        raza:'Criolla',
+        edad:'2 años' 
+    }
+];
+console.log(familia);
+console.log(familia[0]);
+console.log(familia[1]);
+console.log(familia[2]);
+console.log(familia[3]);
+console.log(familia[4]);
+//16
+let textoAleatorio = frutas[1] + numeros[3] + familia[4];
+console.log(textoAleatorio);
+//17
+miEdad = Number(prompt('¿Cúal es mi edad?'));
+let edadCompañero = Number(prompt('¿Cúal es la edad de mi compañero?'));
+//18
+let edadesIguales = miEdad === edadCompañero;
+console.log('Mi edad es igual a la de mi compañero: ' +  edadesIguales);
+//19
+let soyMayor = miEdad > edadCompañero;
+console.log('Mi edad es mayor a la de mi compañero: ' +  soyMayor);
+//20
+let soyMenor = miEdad < edadCompañero;
+console.log('Mi edad es menor a la de mi compañero: ' + soyMenor);
+//21
+let soyMayorDeEdad = miEdad >= 18;
+console.log('Soy mayor de edad: ' + soyMayorDeEdad);
+//22
+let edadNecesaria = 18;
+let alturaNecesaria = 160;
+let puedeSubir = edadNecesaria > 6 && alturaNecesaria >= 120;
+console.log('Puede subir: ' + puedeSubir);
+//23
+let pase = 'VIP';
+let saldo = 1000;
+let puedePasar = pase === 'VIP' || saldo > 1000;
+console.log('La persona puede pasar: ' + puedePasar)
